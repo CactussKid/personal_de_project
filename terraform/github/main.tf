88edf9b638663870b532195062226faa5868838a
CactussKid <drainpreston@gmail.com>
2026-09-19 11:42:36 -0700

    Add qa branch, branch protection, and environments

--- terraform/github/main.tf
+++ terraform/github/main.tf
@@ -24,3 +24,74 @@ resource "github_repository" "this" {
 
   auto_init = false   # <- changed from true
 }
+
+resource "github_branch_protection" "main" {
+  repository_id = github_repository.this.node_id
+  pattern       = "main"
+
+  required_pull_request_reviews {
+    required_approving_review_count = 1
+  }
+
+  required_status_checks {
+    strict   = true
+    contexts = ["ci"]
+  }
+
+  enforce_admins = true
+  allows_force_pushes = false
+}
+
+resource "github_branch" "qa" {
+  repository = github_repository.this.name
+  branch     = "qa"
+}
+
+resource "github_branch_protection" "qa" {
+  repository_id = github_repository.this.node_id
+  pattern       = "qa"
+
+  required_pull_request_reviews {
+    required_approving_review_count = 1
+  }
+
+  allows_force_pushes = false
+
+  depends_on = [github_branch.qa]
+}
+
+resource "github_repository_environment" "qa" {
+  repository  = github_repository.this.name
+  environment = "qa"
+
+  deployment_branch_policy {
+    protected_branches     = false
+    custom_branch_policies = true
+  }
+}
+
+resource "github_repository_environment_deployment_policy" "qa_branch" {
+  repository     = github_repository.this.name
+  environment    = github_repository_environment.qa.environment
+  branch_pattern = "qa"
+}
+
+resource "github_repository_environment" "prod" {
+  repository  = github_repository.this.name
+  environment = "prod"
+
+  reviewers {
+    users = []  # add your GitHub user ID here later if you want manual approval gates
+  }
+
+  deployment_branch_policy {
+    protected_branches     = false
+    custom_branch_policies = true
+  }
+}
+
+resource "github_repository_environment_deployment_policy" "prod_branch" {
+  repository     = github_repository.this.name
+  environment    = github_repository_environment.prod.environment
+  branch_pattern = "main"
+}
